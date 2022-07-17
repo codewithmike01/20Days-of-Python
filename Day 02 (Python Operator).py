a50736b7447973c54b40f95e40cf1902833127a9
ginohmk <michotall95@gmail.com>
2022-07-17 20:07:15 +0100

    Add Comparison Oparators

---
+++ Day 02 (Python Operator).py
@@ -0,0 +1,23 @@
+# Arithemetic operators
+# + Addition
+# / Division
+# * Multiplication
+# - Subtraction
+# ** Power 
+# % Modulus
+
+# Note in pythin you will always get a floating answer 
+# is you use division (/) in python
+print(20/3)
+# Gives 6.666666666666667
+
+# To get just the integer path of a division use a Flow division //
+print(20//3)
+# Gives 6
+
+
+# ## Comparison operator
+# == equality 
+# != Not eqaul
+# > Greater 
+# < Lessthan
