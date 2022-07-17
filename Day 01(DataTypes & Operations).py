17d68db7bad78da27eff1e148c44d2c0c464b7a1
ginohmk <michotall95@gmail.com>
2022-07-17 19:40:39 +0100

    Add dict datatype

--- Day 01(DataTypes & Operations).py
+++ Day 01(DataTypes & Operations).py
@@ -1,6 +1,31 @@
 num = 40
 num2 = 40
-print(id(num))
+
+# The momery address of the varibles will be thesame
+#  because of there hold same values 
+# this id due to Object Intering
+print(id(num), id(num2))
 
 
-print( id(num2))
+
+# Data type we have string, interger and float (Immutable), list, tuplet
+# integer
+num = 90
+
+
+# Float 
+num_second = 32.5
+
+# string
+s = 'Hello boy'
+
+# List: Can store different elemets of  DataType, it is Mutable
+l = [3,2,1,5,'boy']
+print(l)
+
+
+# Tuplet : Can store different elements of data types, it is Immutable
+t = (5, 6, 'mike')
+print(t)
+
+# Dictionary: This is a key and value pair data type
