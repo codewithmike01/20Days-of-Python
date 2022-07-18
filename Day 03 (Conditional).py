d5c5164cd48395b5d7960796a4fdc8aa3002cfaa
ginohmk <michotall95@gmail.com>
2022-07-18 20:49:29 +0100

    Add day 3 condition if

---
+++ Day 03 (Conditional).py
@@ -0,0 +1,12 @@
+# If condition
+
+num = 5
+num2 = 2
+
+if num > num2:
+    print(num , 'is grater than', num2)
+elif num < num2:
+    print(num , 'is less than', num2)
+else:
+    print('Both numbers are equal')
+    
