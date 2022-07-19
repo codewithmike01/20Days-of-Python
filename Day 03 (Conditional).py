50b9177d9d722754c6fbd927390cf22026e614cc
ginohmk <michotall95@gmail.com>
2022-07-19 10:54:04 +0100

    Add while loop

--- Day 03 (Conditional).py
+++ Day 03 (Conditional).py
@@ -15,4 +15,17 @@ else:
 for s in [3,5,7,12,90,2]:
     print(s * 2)
 
+for value in range(5):
+    print(value)
+
+# Use emumerable while working with index
+for value,index in enumerate([2,4,6,7,8]):
+        print(index, value)
+
+
+
 # While loop
+n= 2
+while n <= 10:
+        print('in while loop', n)
+        n += 1
