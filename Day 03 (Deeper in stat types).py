64a0894d50728d222dbd4a71537b0258886e2a67
ginohmk <michotall95@gmail.com>
2022-07-20 08:38:12 +0100

    Add string datatype slicing and indexing

---
+++ Day 03 (Deeper in stat types).py
@@ -0,0 +1,12 @@
+# Strings are Immutable at thesame memory location
+# String is a an ordered data structure
+# It support indexing and slicing
+
+s = 'Python newbie'
+
+print(s[2])
+# Prints t
+
+print(s[0:3]) 
+# Prints Pyt
+
