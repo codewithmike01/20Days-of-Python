3ed1b746303b3b80c9cc65b49f2e6507afbc9f83
ginohmk <michotall95@gmail.com>
2022-07-21 07:39:07 +0100

    Add string capitalize, upper, lower , title all string methods

--- Day 03 (Deeper in stat types).py
+++ Day 03 (Deeper in stat types).py
@@ -25,3 +25,18 @@ print('Value of num1 id {} value of num2 is {}'.format(num, num2))
 
 # Also
 print('Value of num1 id {val1} value of num2 is {val2}'.format(val1 = 50, val2 = 40))
+
+print('upper',s.upper())
+# All element are turned to uppercase
+print('lower',s.lower())
+print('capitalize',s.capitalize())
+# JUst the first letter in the first letter of the word uppercase 
+print('title', s.title())
+# More like camel cases
+
+# ## Slipt and join
+s1 = 'Html, css, javascript'
+print(s1.split(','))
+
+s2 = ['Html', ' css', ' javascript']
+print((' ').join(s2))
