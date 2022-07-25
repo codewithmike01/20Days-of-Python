125b624c360a76fc171c8e297a3c347559cf3421
ginohmk <michotall95@gmail.com>
2022-07-25 11:09:03 +0100

    Add aggregate function arguments

--- Day 05(Function).py
+++ Day 05(Function).py
@@ -1,8 +1,19 @@
 # Fuction helps in code reuse
 # modularity
 
-def sum_num(a,b):
+def sum_num(a,b = 10):
     return a + b 
 
 
-print(sum_num(4,5))
+print(sum_num(4))
+
+# Aggregate arguments
+
+def sum_all(*args):
+        sum = 0
+        for value in args:
+                sum += value
+        return sum
+
+result = sum_all(200,20,100,150,500)
+print(result)
