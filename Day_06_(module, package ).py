541da4467060187031feac8dd8d526f88c2e1e6f
ginohmk <michotall95@gmail.com>
2022-07-25 16:27:53 +0100

    Add regular expression rules

--- Day_06_(module, package ).py
+++ Day_06_(module, package ).py
@@ -19,4 +19,11 @@ print(result)
 # [a-zA-Z] matches a or b or c ... or z A ...B.. or Z
 #  + atleast one character [a - z]+ at least one chracter in a to z
 #  * zero or more characters [A - Z]*
-#  ^ start of a string 
+#  ^ match at the start of a string 
+#  $ match at the end of a string
+#  ? use to specify optional
+#  {} specify nummber of ocurances [a-z]{4} expect 4 occurances of  character
+  #  [a - z]{2,4} expect atleast 2 occurances or atmost 4
+
+
+  #  ## examples 
