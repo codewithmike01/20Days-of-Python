4d3fa5df98c579bd95166646b2f289211209d60f
ginohmk <michotall95@gmail.com>
2022-07-25 16:24:16 +0100

    Add regular expression rules

--- Day_06_(module, package ).py
+++ Day_06_(module, package ).py
@@ -1,3 +1,4 @@
+import re
 import test_module
 
 
@@ -9,3 +10,13 @@ print(result)
 # __init__
 
 # Regular expression
+
+# you need to import re
+
+
+#  . aline with any character
+#  [] to specify range [a - z] , [A - Z], [0 - 9]
+# [a-zA-Z] matches a or b or c ... or z A ...B.. or Z
+#  + atleast one character [a - z]+ at least one chracter in a to z
+#  * zero or more characters [A - Z]*
+#  ^ start of a string 
