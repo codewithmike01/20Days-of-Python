813c1bfe6d380c261a0aa28fd83114f434d7283f
ginohmk <michotall95@gmail.com>
2022-07-25 11:27:57 +0100

    Add day 06 module and package

--- Day 05(Function).py
+++ Day 05(Function).py
@@ -8,6 +8,7 @@ def sum_num(a,b = 10):
 print(sum_num(4))
 
 # Aggregate arguments
+# arge as in Tuple datatype
 
 def sum_all(*args):
         sum = 0
@@ -17,3 +18,11 @@ def sum_all(*args):
 
 result = sum_all(200,20,100,150,500)
 print(result)
+
+# Keyword aarguments
+# Are in dictionary data type
+
+def employee_info(**empy):
+    print('{} is {} years old and earns a monthly salary of {}'.format(empy['name'], empy['age'],empy['salary'])) 
+
+employee_info(name = 'Mike', age = 23, salary = '$45000')
