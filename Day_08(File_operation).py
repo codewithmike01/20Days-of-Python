334d1fe65fa93cfa67e7e3213a85096d1da8bc94
ginohmk <michotall95@gmail.com>
2022-07-26 18:00:26 +0100

    Add file read and readline

--- Day_08(File_operation).py
+++ Day_08(File_operation).py
@@ -10,5 +10,11 @@
 
 file = open('text_a.txt','r')
 content = file.read()
+
+# To specify the 
+# number of characters needed to be read
+# file.read(20) thats 20 character;
 print(content)
 
+# Using read gets the text as string
+# Using readline() prints each line separated by a newline character \n
