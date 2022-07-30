ad752c170a0da1552a2e4dadc486afa8132dded1
ginohmk <michotall95@gmail.com>
2022-07-30 12:35:34 +0100

    Add oop methode and variable

---
+++ Day_10(intor_OOP).py
@@ -0,0 +1,34 @@
+
+# defining a class
+class Account:
+    def __init__(self, cust_id, name, initial_bal= 0):
+          self.id = cust_id
+          self.name = name
+          self.balance = initial_bal
+
+    def get_balance(self):
+        return self.balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+    
+    def withdraw(self, amount):
+        if (amount > self.balance):
+            return 'You balance is lesser'
+        else:
+            self.balance -= amount
+            return 'deposited sucessfully'
+
+# crating object for the class
+
+customer1 = Account(1, 'John')
+customer2 = Account(2, 'Frank')
+customer3 = Account(3, 'Samson')
+
+print(customer1.id)
+
+customer1.deposit(300)
+print(customer1.get_balance())
+result = customer1.withdraw(50)
+print(result)
+print(customer1.get_balance())
