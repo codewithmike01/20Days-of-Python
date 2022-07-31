8a082d832bccd6872fe86313885f7843c39221b6
ginohmk <michotall95@gmail.com>
2022-07-31 21:18:24 +0100

    Add soup pretify

--- Day13(Web_scraping).py
+++ Day13(Web_scraping).py
@@ -4,4 +4,19 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get("https://www.jumia.com.ng/?aff_click_id=441b8f3f-a557-471c-aacc-7d4f9ff60952&utm_source=kol&utm_medium=affiliation&utm_campaign=43b01fb3-23b0-4c5a-9d44-b1f0b80f5af7&utm_term=customlinks&utm_content=189f6752-f881-37f4-87a4-7cae442bf3ee")
-print(response)
+
+
+#  To get the staus respones
+print(response.status_code)
+# To get the content
+# print(response.content)
+
+# To get it as html
+
+soup = BeautifulSoup(response.content,'html.parser')
+print(soup.prettify())
+
+# Find will find the first encounter of the elememt specify
+# find('a') -> Finds the first anchor tag
+# find_all('img;) -> Finds all image tags
+# find_parent('img') -> Finds parent tag
