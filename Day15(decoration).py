494c0adcf6be580bd6ef03cb30d2a815b562a8fe
ginohmk <michotall95@gmail.com>
2022-08-03 21:52:31 +0100

    Add Decoration function

--- Day15(decoration).py
+++ Day15(decoration).py
@@ -1,6 +1,14 @@
 # it takes fuction as arguments and returns a function
 
+def deco(func):
+    def new_func(val1, val2):
+        if type(val1) == type(val2):
+            return func(val1,val2)
+        else:
+            return func(str(val1),str(val2))
+    return new_func
 
+@deco
 def concat(val1, val2):
     return val1 + val2
 result = concat(10,10)
@@ -10,7 +18,7 @@ print(result)
 result = concat('hello','world')
 print(result)
 
-result = concat('[rint',10)
+result = concat('rint',10)
 print(result)
 
 
